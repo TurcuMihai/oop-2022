208cf1d53bc9bff1465d06dc872068a14e2f8c0b
Turcu Mihai <mihaiturcu35m@gmail.com>
2022-02-27 20:47:07 +0200

    Lab01_problema02_OOP

---
+++ Lab01_problema02_OOP.cpp
@@ -0,0 +1,56 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include <iostream>
+#include <stdio.h>
+#define BUFFER_SIZE 100
+using namespace std;
+
+int my_atoi(char number[])
+{
+    int suma = 0, numar_valid = 1;
+    for (int i = 0; i < strlen(number)-1; i++)              // Am pus strlen(number) - 1 deoarece imi lua si 
+    {                                                       //      ENTER-ul de dupa fiecare numar in considerare
+        
+        if (number[i] >= '0' && number[i] <= '9')
+        {
+            suma = suma * 10 + (number[i] - '0');
+        }
+        else
+        {
+            numar_valid = 0;
+        }
+        
+    }
+    if (numar_valid == 1)
+    {
+        return suma;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+int main()
+{
+    FILE* fisier;
+    char sir[100];
+    int rezultat = 0, numar;
+    
+    if ((fisier = fopen("in.txt", "r")) == NULL)
+    {
+        printf("Eroare la deschiderea fisierului.");
+        exit(1);
+    } 
+    
+    while (fgets(sir, BUFFER_SIZE, fisier) != NULL)
+    {
+        numar = my_atoi(sir);
+        if (numar != -1)
+        {
+            rezultat += numar;
+        }
+    }
+
+    printf("%d", rezultat);
+    return 0;
+}
